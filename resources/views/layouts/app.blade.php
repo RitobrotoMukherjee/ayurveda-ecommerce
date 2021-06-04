abc54afd060a2335d249a737656ecdfe3bfef3a9
RitoM <ritobrotomukherjee1991@gmail.com>
2021-06-04 14:16:05 +0530

    app css version change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ config('app.asset_url') }}/css/app.css" rel="stylesheet">
+    <link href="{{ config('app.asset_url') }}/css/app-v1.0.1.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
