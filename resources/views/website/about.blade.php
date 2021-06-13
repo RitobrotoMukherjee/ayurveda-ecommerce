2be91ff36c926c737b0de2a438d9e10ae4a30dcd
RitoM <ritobrotomukherjee1991@gmail.com>
2021-06-13 22:15:18 +0530

    About us page UTF-8 encoding issue fix

--- resources/views/website/about.blade.php
+++ resources/views/website/about.blade.php
@@ -20,10 +20,10 @@
                 <div class="col-md-6">
                     <div class="left-content">
                         <h4>Dr. Peddinti Rangacharyulu</h4>
-                        <h4>అనువంశిక వైద్యులు(Hereditary Physician)</h4>
-                        <h5>Jananm : in 1865</h5>
-                        <h5>Maranam: 1945</h5>
-                        <p>He Established Sree Krishna Ayurvedic Pharmacy in 1918 (Great Grandfather of the present Proprietor Sri Peddinti Venugopal) Carried out Manufacturing of Ayurvedic Medicines useful for the Mankind. Some of the Patented Medicines also spreaded all over the World.  For this he was awarded â€œRAJAVAIDYAâ€� by the Local British Administration during 1930â€™s.   Trade Mark Registration done in Britishers time .</p>
+                        <h4>(Hereditary Physician)</h4>
+                        <h5>Born : in 1865</h5>
+                        <h5>Expired: 1945</h5>
+                        <p>He Established Sree Krishna Ayurvedic Pharmacy in 1918 (Great Grandfather of the present Proprietor Sri Peddinti Venugopal) Carried out Manufacturing of Ayurvedic Medicines useful for the Mankind. Some of the Patented Medicines also spreaded all over the World.  For this he was awarded RAJAVAIDYA by the Local British Administration during 1930's.   Trade Mark Registration done in Britishers time .</p>
                     </div>
                 </div>
             </div>
@@ -36,8 +36,8 @@
                 <div class="col-md-6">
                     <div class="left-content">
                         <h4>Dr. Peddinti Venkatacharyulu  L.I.M.</h4>
-                        <h5>Jananm : in 1905</h5>
-                        <h5>Maranam: 1992</h5>
+                        <h5>Born : in 1905</h5>
+                        <h5>Born: 1992</h5>
                         <p>He completed L.IM and started Ayurvedic Medical practice at Eluru and continued to develop SREE KRISHNAYURVEDIC PHARMACY with additional Manufacturing Units having  branches in MADRAS, Calcutta, Vijayawada etc. through out India.He served as Vice President for AYURVEDIC VAIDYA MAHA MANDALI, India  For 50 years </p>
                     </div>
                 </div>
@@ -51,9 +51,9 @@
                 <div class="col-md-6">
                     <div class="left-content">
                         <h4>Dr. Peddinti Rangacharyulu  B.A.M.S</h4>
-                        <h4>AYURVEDIC MEDICAL OFFICERS ASSOCIATION, Govt. A.P.</h4>
-                        <h5>Jananm : in 1935</h5>
-                        <h5>Maranam: 2019</h5>
+                        <h4>AYURVEDIC MEDICAL OFFICER, Govt. A.P.</h4>
+                        <h5>Born : in 1935</h5>
+                        <h5>Born: 2019</h5>
                         <p>He Completed B.A.M.S.  and joined in Govt. Service at Ayurvedic Hospital, Chirala.  Continuously guiding his son and present proprietor of Sree Krishna Ayurvedic Pharmacy Sri Peddinti Venugopal in preparation of Medicines for various diseases also made his own presense for attending serious cases at eluru whenever required.  He elected as Vice President for State Municipal AYURVEDIC MEDICAL OFFICERS ASSOCIATION and served for many years for the welfare of the Ayurvedic Doctors spreaded all over in India.</p>
                     </div>
                 </div>
@@ -84,7 +84,7 @@
                     <div class="service-item">
                         <div class="icon">
                             <img src="{{ config('app.asset_url') }}/assets/images/skap-team/P-Sreenivasacharyulu.png" width="250" height="350" alt="">
-                            <h4>P Sreenivasacharyulu B.A.M.S</h4>
+                            <h4>P Srinivasacharyulu B.A.M.S</h4>
                         </div>
                     </div>
                 </div>
@@ -128,9 +128,9 @@
                 <br>
                 <p>Presently Dealing in Import and Export of Ayurvedic medicines, Herbs, Herbal Extracts and Raw material for all the needy in all over the World.</p>
                 <br>
-                <p>Sree Krishna Ayurvedic Pharmacy is one of the leading Ayurvedic pharmaceutical manufacturing companies in the state of Andhra Pradesh, based in Eluru, West Godavari District. The company came into existence in 1918. The Family has been involved in the Ayurvedic Herbal Healthcare and Pharmaceuticals since 1918. We here at SREE Krishna Ayurvedic Pharmacy as a team are inspired from the ethics laid by our Great Grand Father â€“ Dr. Sri Peddinti Rangacharyulu, who had the strong vision of serving the mankind. Our work ethics have been nurtured under the guidance of our grandfather â€“ Dr. Sri Peddinti Venkatacharyulu and my Father Dr. Sri Peddinti Rangacharyulu, who has inculcated in us the spirits of diligence, honesty, dedication and Integrity and laid the foundation of this healthcare service for mankind.</p>
+                <p>Sree Krishna Ayurvedic Pharmacy is one of the leading Ayurvedic pharmaceutical manufacturing companies in the state of Andhra Pradesh, based in Eluru, West Godavari District. The company came into existence in 1918. The Family has been involved in the Ayurvedic Herbal Healthcare and Pharmaceuticals since 1918. We here at SREE Krishna Ayurvedic Pharmacy as a team are inspired from the ethics laid by our Great Grand Father Dr. Sri Peddinti Rangacharyulu, who had the strong vision of serving the mankind. Our work ethics have been nurtured under the guidance of our grandfather Dr. Sri Peddinti Venkatacharyulu and my Father Dr. Sri Peddinti Rangacharyulu, who has inculcated in us the spirits of diligence, honesty, dedication and Integrity and laid the foundation of this healthcare service for mankind.</p>
                 <br>
-                <p>Undoubtedly, we here at Sree Krishna Ayurvedic Pharmacy, under the blessing of our forefathers aim for a healthier living of standards of human beings. With the â€˜Dream of healthy Indiaâ€™ and the mankind everywhere else in the world, we desire that every home equip herbal wellness as their Health Matra for Healthy Living. We wish to attain the status of the most trusted company in our genre of Ayurvedic Herbal Healthcare and most respected for our quality, ethical values and assurance to sustainability. We at Sree Krishna Ayurvedic Pharmacy believe in committed to integrity, accountability, fairness, transparency and innovation.</p>
+                <p>Undoubtedly, we here at Sree Krishna Ayurvedic Pharmacy, under the blessing of our forefathers aim for a healthier living of standards of human beings, with the dream of healthy India and the mankind everywhere else in the world, we desire that every home equip herbal wellness as their Health Matra for Healthy Living. We wish to attain the status of the most trusted company in our genre of Ayurvedic Herbal Healthcare and most respected for our quality, ethical values and assurance to sustainability. We at Sree Krishna Ayurvedic Pharmacy believe in committed to integrity, accountability, fairness, transparency and innovation.</p>
                 <br>
                 <p>We're building on our history of hard work, diligence and success, by working tirelessly to enhance the value we are committed to provide towards improving health and the quality of our lives, across the world. Whether you are a consumer, healthcare professional or investor, please consider our website as your personal resource for learning more about Sree Krishna Ayurvedic Pharmacy and exploring our company. We appreciate your interest in our company, its product and its people.</p>
 
